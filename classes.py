23952d479ec8773bc1cbcca3cdc211906810efc4
Joseph Jess <X00242409@linnbenton.edu>
2020-10-07 14:16:55 -0700

    Add settings.json for VSCode, minor update to classes.py

--- classes.py
+++ classes.py
@@ -7,27 +7,35 @@
         fix git repositories
 """
 
+
 class Person:
     """Defines a Person."""
+
     def __init__(self):
-        """Initializes a Person object."""
-        self.name:str = "Joseph"
-        self.age:int = 34
-        self.is_alive:int = True
+        """Initialize a Person object."""
+
+        self.name: str = "Joseph"
+        self.age: int = 34
+        self.is_alive: int = True
 
     def double_age(self):
+        """Return double the value of this person's age.."""
         return self.age * 2
 
     def birthday(self):
+        """Increase this peron's age by 1."""
         self.age += 1
 
-    def birthdays(self, num_birthdays:int):
+    def birthdays(self, num_birthdays: int):
+        """Increase this peron's age by num_birthdays."""
         self.age += num_birthdays
 
         return self.age
 
     def print_yo_self(self):
+        """Print this person in a human readable format."""
         print(f"name: {self.name}, age: {self.age}, is_alive: {self.is_alive}")
+
 
 joseph = Person()
 # print(f"joseph.name: {joseph.name}")
